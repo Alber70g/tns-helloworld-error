45b8774cf09256ec6b886c171682d4b57e13e37b
Albert Groothedde <albertgroothedde@gmail.com>
2015-06-19 14:58:56 +0200

    initial

---
+++ platforms/ios/build/helloworld.build/Debug-iphonesimulator/helloworld.build/Objects-normal/i386/main.d
@@ -0,0 +1,5 @@
+dependencies: /Users/albert/helloworld/platforms/ios/helloworld/main.m \
+  /Users/albert/helloworld/platforms/ios/NativeScript/include/NativeScript.h \
+  /Users/albert/helloworld/platforms/ios/NativeScript/include/TNSRuntime.h \
+  /Users/albert/helloworld/platforms/ios/NativeScript/include/TNSRuntime+Inspector.h \
+  /Users/albert/helloworld/platforms/ios/helloworld/TNSDebugging.h
